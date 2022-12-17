3239972ca2ca536f3f70fb1dbffcd0073095182b
gana <gananathanthirunavukkarasu@gmail.com>
2022-12-17 13:30:04 +0530

    get user completed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient } = require("mongodb");
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -15,10 +15,9 @@ app.get("/", (req, res) => {
   res.json("Tinder app");
 });
 
-const MONGO_URL  = process.env.MONGO_URL;
+const MONGO_URL = process.env.MONGO_URL;
 
 app.post("/signup", async (req, res) => {
-
   const client = new MongoClient(MONGO_URL);
 
   const { email, password } = req.body; //taking email and password as input
@@ -27,7 +26,7 @@ app.post("/signup", async (req, res) => {
 
   try {
     await client.connect();
-    console.log("mongodb connected")
+    console.log("mongodb connected");
     // finding data to see if it has the email already
     const userExist = await client
       .db("app-data")
@@ -52,48 +51,44 @@ app.post("/signup", async (req, res) => {
       .collection("users")
       .insertOne(data);
 
-      // create token
+    // create token
     const token = jwt.sign(insertUser, sanitizedEmail, { expiresIn: 60 * 24 });
-    res.status(201).json({token,userId:generateUserId});
-  } 
-  catch (error) {
+    res.status(201).json({ token, userId: generateUserId });
+  } catch (error) {
     res.status(500).json({ Message: "Something went wrong" });
     console.log(error);
   }
-
 });
 
 app.post("/login", async (req, res) => {
   const client = new MongoClient(MONGO_URL);
-  const {email,password} = req.body;
+  const { email, password } = req.body;
 
   try {
     await client.connect();
     const user = await client
       .db("app-data")
       .collection("users")
-      .findOne({email});
+      .findOne({ email });
 
-      const correctPassword = await bcrypt.compare(password,user.hashed_passedword)
-      if(user && correctPassword){
-        const token = jwt.sign(user, email, { expiresIn: 60 * 24 });
-        res.status(201).json({token,userId:user.user_id})
-      }
-      else{
-        res.status(400).send("Invalid credentials");
-      }
-
-  }
-   catch (error) {
+    const correctPassword = await bcrypt.compare(
+      password,
+      user.hashed_passedword
+    );
+    if (user && correctPassword) {
+      const token = jwt.sign(user, email, { expiresIn: 60 * 24 });
+      res.status(201).json({ token, userId: user.user_id });
+    } else {
+      res.status(400).send("Invalid credentials");
+    }
+  } catch (error) {
     console.log(error);
   }
-
-})
+});
 
 app.get("/users", async (req, res) => {
-
   const client = new MongoClient(MONGO_URL);
-  
+
   try {
     await client.connect();
     const returnedUsers = await client
@@ -102,49 +97,68 @@ app.get("/users", async (req, res) => {
       .find()
       .toArray();
     res.send(returnedUsers);
-  }
-   catch (error) {
+  } catch (error) {
     res.status(500).json({ Message: "Something went wrong" });
     console.log(error);
   }
-
 });
 
-app.put("/user", async(req,res)=>{
+app.get("/user/:userId", async (req, res) => {
   const client = new MongoClient(MONGO_URL);
-  const formData = req.body.formData
+  const userId = req.params.userId;
+
+  console.log(req.params);
+  console.log(userId)
 
   try {
     await client.connect();
-    const database = await client
+    const user = await client
       .db("app-data")
-      .collection("users");
-console.log(formData.user_id)
-      const query = {user_id:formData.user_id}
+      .collection("users")
+      .findOne({ user_id:userId });
 
-      const updateDocument = {
-        $set : {
-          first_name:formData.first_name,
-          dob_day:formData.dob_day,
-          dob_month:formData.dob_month,
-          dob_year:formData.dob_year,
-          show_gender:formData.show_gender,
-          gender_identity:formData.gender_identity,
-          gender_interest:formData.gender_interest,
-          url:formData.url,
-          about:formData.about,
-          matches:formData.matches
-        }  
-      }
-      console.log(updateDocument)
-      const insertedUser = await database.updateOne(query,updateDocument)
+    // const database = await client.db("app-data").collection("users");
+    // console.log(database);
+    // const query = { userId: userId };
+    // const user = database.findOne(query);
+    res.send(user);
+
+  } finally {
+    await client.close();
+  }
+});
+
+app.put("/user", async (req, res) => {
+  const client = new MongoClient(MONGO_URL);
+  const formData = req.body.formData;
+
+  try {
+    await client.connect();
+    const database = await client.db("app-data").collection("users");
+    console.log(formData.user_id);
+    const query = { user_id: formData.user_id };
+
+    const updateDocument = {
+      $set: {
+        first_name: formData.first_name,
+        dob_day: formData.dob_day,
+        dob_month: formData.dob_month,
+        dob_year: formData.dob_year,
+        show_gender: formData.show_gender,
+        gender_identity: formData.gender_identity,
+        gender_interest: formData.gender_interest,
+        url: formData.url,
+        about: formData.about,
+        matches: formData.matches,
+      },
+    };
+    console.log(updateDocument);
+    const insertedUser = await database.updateOne(query, updateDocument);
     res.send(insertedUser);
+  } finally {
+    await client.close();
   }
-   finally{
-   await client.close();
-  }
-  
-})
+});
 
 app.listen(PORT, () => {
   console.log(`app started in ${PORT}`);
